ef456d77051b33847fa2dd524bd275d9642a3290
kylechenoO <kyle@hacking-linux.com>
2019-03-29 14:41:16 +0800

    fix webpack-dev-server warning

--- webapp/frontend/config/index.js
+++ webapp/frontend/config/index.js
@@ -15,6 +15,7 @@ module.exports = {
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    disableHostCheck: true,
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
