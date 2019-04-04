8fdbdf7a9cc1aa2255d93dfaa8244dc0bff4eadd
kylechenoO <kyle@hacking-linux.com>
2019-04-04 22:01:22 +0800

    split signin signup css

--- WebApp/views/SignUpForm.py
+++ WebApp/views/SignUpForm.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 class SignUpForm(FlaskForm):
     user_name = StringField('User Name', validators = [DataRequired()], render_kw = {'placeholder': 'USERNAME',
         'onfocus': "this.placeholder = '';",
         'onblur': "if (this.placeholder == '') {this.placeholder = 'USERNAME';}"})
-    email = StringField('Email', validators = [DataRequired()], render_kw = {'placeholder': 'EMAIL',
+    email = EmailField('Email', validators = [DataRequired()], render_kw = {'placeholder': 'EMAIL',
         'onfocus': "this.placeholder = '';",
         'onblur': "if (this.placeholder == '') {this.placeholder = 'EMAIL';}"})
     password = PasswordField('Password', validators = [DataRequired()], render_kw = {'placeholder': 'PASSWORD',
