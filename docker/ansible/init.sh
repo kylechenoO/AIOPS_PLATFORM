274a98871a40b42a13bbd6003e89df1db2dcfbce
kylechenoO <kyle@hacking-linux.com>
2019-04-14 17:06:04 +0800

    map timezone file into container

--- docker/ansible/init.sh
+++ docker/ansible/init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build . -t aiops_ansible_img
-docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --network mynet --ip 1.1.1.2 --dns 223.5.5.5 --dns 223.6.6.6 --name ansible --hostname ansible -tdi aiops_ansible_img:latest /usr/sbin/init
+docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --network mynet --ip 1.1.1.2 --dns 223.5.5.5 --dns 223.6.6.6 --name ansible --hostname ansible -tdi aiops_ansible_img:latest /usr/sbin/init
