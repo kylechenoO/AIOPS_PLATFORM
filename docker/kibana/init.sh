c9def4358f1f65d01ac22d649bcf7045fd069709
kylechenoO <kyle@hacking-linux.com>
2019-04-28 21:13:07 +0800

    initial elasticsearch and kibana docker init script

---
+++ docker/kibana/init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --network mynet --ip 1.1.1.7 --dns 223.5.5.5 --dns 223.6.6.6 -d --link elasticsearch:elasticsearch -p 5601:5601 --hostname kibana --name kibana docker.elastic.co/kibana/kibana:7.0.0
