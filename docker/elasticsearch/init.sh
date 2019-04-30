d01af29301ff1e2b28453bb776fc8b65424b4592
docs <kyle@hacking-linux.com>
2019-04-30 21:52:35 +0800

    commit mariadb/init.sh

--- docker/elasticsearch/init.sh
+++ docker/elasticsearch/init.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-# TODO: STILL TRYING SYSLOG
 docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --network mynet --ip 1.1.1.6 --dns 223.5.5.5 --dns 223.6.6.6 -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --hostname elasticsearch --name elasticsearch docker.elastic.co/elasticsearch/elasticsearch:7.0.0
