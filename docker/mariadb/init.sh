7c627599b1db586881f84f7cc70bfdea8b13b898
kylechenoO <kyle@hacking-linux.com>
2019-04-30 17:22:14 +0800

    add filebeat to mariadb container

--- docker/mariadb/init.sh
+++ docker/mariadb/init.sh
@@ -3,3 +3,11 @@ docker build . -t aiops_mariadb_img
 docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --network mynet -e MYSQL_ROOT_PASSWORD=21478De23a1a1628c944 --ip 1.1.1.5 --dns 223.5.5.5 --dns 223.6.6.6 -d -p 3306:3306 --hostname mariadb --name mariadb aiops_mariadb_img:latest
 sleep 10
 docker exec -i mariadb mysql -uroot -p21478De23a1a1628c944 < ./db/aiops.ddl
+docker exec -i mariadb apt install curl rsyslog -y
+docker exec -i mariadb service rsyslog start
+docker exec -i mariadb curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.0.0-amd64.deb
+docker exec -i mariadb dpkg -i filebeat-7.0.0-amd64.deb
+docker cp src/filebeat.yml mariadb:/etc/filebeat/filebeat.yml
+docker exec -i mariadb filebeat modules enable system
+docker exec -i mariadb filebeat setup
+docker exec -i mariadb service filebeat start
