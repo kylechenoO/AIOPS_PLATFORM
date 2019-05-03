c39d534d39f73af77f457035757f8b896f411e47
Kyle Chen <kyle@hacking-linux.com>
2019-05-03 22:34:22 +0800

    use docker --log-driver syslog to collect logs from container

--- docker/kibana/init.sh
+++ docker/kibana/init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --network mynet --ip 1.1.1.7 --dns 223.5.5.5 --dns 223.6.6.6 -d --link elasticsearch:elasticsearch -p 5601:5601 --hostname kibana --name kibana docker.elastic.co/kibana/kibana:7.0.0
+docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /etc/localtime:/etc/localtime:ro --log-driver=syslog --log-opt tag=kibana --network mynet --ip 1.1.1.7 --dns 223.5.5.5 --dns 223.6.6.6 -d --link elasticsearch:elasticsearch -p 5601:5601 --hostname kibana --name kibana docker.elastic.co/kibana/kibana:7.0.0
