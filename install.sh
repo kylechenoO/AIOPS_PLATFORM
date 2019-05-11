7dd3bd5c4b8ff640ee6c2f19416adede95557bd5
kylechenoO <kyle@hacking-linux.com>
2019-05-11 22:56:23 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+if [ $(pwd) != "/AIOPS" ]
+then
+
+    echo "Please put this directory in '/', and rename it as '/AIOPS'."
+    exit -1
+
+fi
+
+if [ -f /etc/redhat-release ]
+then
+    yum clean all
+    yum makecache
+    yum upgrade -y
+    yum install vim openssh-server epel-release gcc glibc wget bzip2 dmidecode -y
+    wget -c http://hacking-linux.oss-cn-hongkong.aliyuncs.com/tools/Anaconda3-2019.03-Linux-x86_64.sh -O /opt/Anaconda3-2019.03-Linux-x86_64.sh
+    bash /opt/Anaconda3-2019.03-Linux-x86_64.sh -b -p /opt/Anaconda3
+    echo "set -o vi" >> /root/.bashrc
+    echo "export LD_LIBRARY_PATH=/opt/Anaconda3/lib/:$LD_LIBRARY_PATH" >> /root/.bashrc
+    echo "export PATH=/opt/Anaconda3/bin/:$PATH" >> /root/.bashrc
+    FLAG=$(crontab -l | awk 'BEGIN{ result = "False"; }/^#/{ next; }{ if($0 == "* * * * * /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py &> /dev/null &"){ result = "True"; } }END{ printf("%s", result); }')
+    if [ ${FLAG} == False ]
+    then
+        crontab -l > conf
+        echo '* * * * * /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py &> /dev/null &' >> conf
+        crontab conf
+        /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py
+
+    fi
+
+else
+    apt update
+    apt upgrade -y
+    apt install wget dmidecode gcc -y
+    wget -c http://hacking-linux.oss-cn-hongkong.aliyuncs.com/tools/Anaconda3-2019.03-Linux-x86_64.sh -O /opt/Anaconda3-2019.03-Linux-x86_64.sh
+    bash /opt/Anaconda3-2019.03-Linux-x86_64.sh -b -p /opt/Anaconda3
+    echo "set -o vi" >> /root/.bashrc
+    echo "export LD_LIBRARY_PATH=/opt/Anaconda3/lib/:$LD_LIBRARY_PATH" >> /root/.bashrc
+    echo "export PATH=/opt/Anaconda3/bin/:$PATH" >> /root/.bashrc
+    FLAG=$(crontab -l | awk 'BEGIN{ result = "False"; }/^#/{ next; }{ if($0 == "* * * * * /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py &> /dev/null &"){ result = "True"; } }END{ printf("%s", result); }')
+    if [ ${FLAG} == False ]
+    then
+        crontab -l > conf
+        echo '* * * * * /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py &> /dev/null &' >> conf
+        crontab conf
+        /opt/Anaconda3/bin/python /AIOPS/Scheduler/bin/Scheduler.py
+
+    fi
+
+fi
